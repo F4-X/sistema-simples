a6398a5b54f8dff65d661f50ce96a9c46ce08d1d
F4chini <f4xcore@gmail.com>
2026-06-06 12:12:41 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,10 +6,13 @@ dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 export async function query(text, params) {
-  const result = await pool.query(text, params);
-  return result;
+  return pool.query(text, params);
 }
