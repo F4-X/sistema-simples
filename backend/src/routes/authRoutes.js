8019837fcf9c6384ca475e3afd50cfc158949521
F4chini <f4xcore@gmail.com>
2026-06-06 12:03:27 -0300

    asda

--- backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { cadastrar, login } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/cadastro', cadastrar);
-router.post('/login', login);
-
-module.exports = router;
